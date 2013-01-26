9ac1240e60a78d1d4fa5c742a560447919e8ec3b
Sundeep <sundeepisat@gmail.com>
2013-01-25 17:39:18 -0800

    updated extension

--- deadline.js
+++ deadline.js
@@ -1,11 +1,10 @@
 $(document).ready( function() {
 	
-	// defaults
-	
 	// bindings
-	$('#set_values').bind('click', addDeadline);
+    $('#set_values').bind('click', addDeadline);
+    $('#d_list li a').live('click', removeDeadline);
 	
-	// list current deadlines
+	// setup defaults
 	listCurrentDeadlines();
 });
 
@@ -16,18 +15,25 @@ var addDeadline = function() {
 	var title = $("#title_value").val();
 	var date = new Date($("#datepicker").val());
 	var diff = calculate_days(date);
-	
-	// add to localstorage
-	addItemToLocalStorage(title,date);
-	
+	var deadline = { "title": title, "deadline": date };
+
+    // add to localstorage
+	var id = addItemToLocalStorage(deadline);
+
 	// add to view
-	$("#d_list").append("<li> " + title + " : " + diff + " days left </li>");
+	$("#d_list").append(lineItem(deadline,id));
 	
 	// clear out fields 
-	$("#title_value").val();
+	$("#title_value").val("");
+	$("#datepicker").val(getTodaysDate());
 	
 };
 
+var removeDeadline = function () {
+    console.info("deleting!!");
+    localStorage.removeItem($(this).parent().attr("id"));
+    $(this).parent().slideUp('slow', function () { $(this).remove(); });
+};
 
 var calculate_days = function(picked_date) {
 	var now = new Date();
@@ -36,17 +42,53 @@ var calculate_days = function(picked_date) {
 }
 
 var listCurrentDeadlines = function() {
-	
+    var current_count = parseInt(localStorage.getItem("current_count"));
+    console.info("current_count:" + current_count);
+    if (current_count > 0) {
+        for (i = 0; i < current_count; i++) {
+            var item = JSON.parse(localStorage.getItem("deadline-" + i));
+            if (!item) continue;
+            console.info("item:deadline-" + i + " == " + item);
+            $("#d_list").append(lineItem(item, i));
+            //alert("item:" + item);
+        }
+    }
 };
 
+var addItemToLocalStorage = function(deadline_obj) {
 
-var addItemToLocalStorage = function(name,date) {
-	var current_count = parseInt(localStorage.getItem("current_count"));
-	if(!current_count) {current_count=1; alert ("zero count!");}
+    var name = deadline_obj.title;
+    var date = deadline_obj.deadline;
+
+    var current_count = parseInt(localStorage.getItem("current_count"));
+	if(!current_count) {current_count=0; alert ("zero count!");}
 	var deadline = {"title":name, "deadline":date};
-	var name = "deadline-" + current_count;
-	localStorage.setItem(name,JSON.stringify(deadline));
+	var idfy = "deadline-" + current_count;
+	localStorage.setItem(idfy, JSON.stringify(deadline));
 	current_count += 1;
 	localStorage.removeItem("current_count");
 	localStorage.setItem("current_count", current_count);
+	return current_count;
+};
+
+var lineItem = function (deadline_obj, i) {
+    console.info("InLineItem:- deadline_obj=" + JSON.stringify(deadline_obj));
+    var li = "<li id=deadline-" + i + ">" + deadline_obj.title + ":" + calculate_days(new Date(deadline_obj.deadline)) + " days left <a href='#'>x</a> </li>";
+
+    console.info("lineitem: " + li);
+    return li;
+}
+
+
+var getTodaysDate = function() {
+    var now = new Date();
+    var month = (now.getMonth() + 1);
+    var day = now.getDate();
+    if (month < 10)
+        month = "0" + month;
+    if (day < 10)
+        day = "0" + day;
+    var today = now.getFullYear() + '-' + month + '-' + day;
+
+    return today;
 };
